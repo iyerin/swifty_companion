be26783010e09c407e36ce4cb8e5ab0628b3a4b1
Ihor YERIN <ierinigor@gmail.com>
2019-05-09 15:36:41 +0300

    Initial Commit

---
+++ swifty_companion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swifty_companion
+//
+//  Created by Ihor YERIN on 09/05/2019.
+//  Copyright © 2019 Ihor YERIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
