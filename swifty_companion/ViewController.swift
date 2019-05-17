20215c7a6d16347b17c64af130a1c71dc45961c1
Ihor YERIN <ierinigor@gmail.com>
2019-05-17 19:20:14 +0300

    123

--- swifty_companion/ViewController.swift
+++ swifty_companion/ViewController.swift
@@ -13,26 +13,15 @@ class ViewController: UIViewController {
     var myUser = UserData.shared.myUser
     
     @IBAction func serchButton(_ sender: UIButton) {
-        
-//        DispatchQueue.main.async {
-//            //login
-//            //token
-//            //userdata
-//        }
-        
-        
         if let login = loginLabel.text {
-            
             UserData.shared.myUser.login = login
             getToken()
+            activity.isHidden = false
+            activity.startAnimating()
         } else { return }
     }
     @IBOutlet weak var loginLabel: UITextField!
-    
-//    func go() {
-//        performSegue(withIdentifier: "ToUser", sender: self)
-//    }
-    func go (completionH)
+    @IBOutlet weak var activity: UIActivityIndicatorView!
     
     func getToken() {
         let url = ("https://api.intra.42.fr/oauth/token").addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
@@ -49,7 +38,7 @@ class ViewController: UIViewController {
             else if let d = data {
                 do {
                     if let json = try JSONSerialization.jsonObject(with: d) as? [String: Any] {
-                        print("auth: ", json)
+                        //print("auth: ", json)
                         UserData.shared.token = (json["access_token"] as! String?)!
                         let url = ("https://api.intra.42.fr/v2/users/" + UserData.shared.myUser.login).addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
                         let escapedUrl = URL(string: url!)
@@ -67,21 +56,33 @@ class ViewController: UIViewController {
                             }
                             else if let d = data {
                                 do {
-                                    let json = try JSONSerialization.jsonObject(with: d)
+                                    let json = try JSONSerialization.jsonObject(with: d) as! [String: Any]
+                                   // print(json)
                                     print("tyt")
-                                    //print (json)
                                     
-                                    let jsonArray = json as? [String: Any]
-                                    if let photo = jsonArray!["image_url"]
-                                    {
+                                    if let photo = json["image_url"] {
                                         UserData.shared.myUser.photo = photo as! String
-                                        print("name = ", photo)
-                                        
-                                        ///////////////// fill all data HERE
                                     }
-                                    //self.go()
-                                   // self.performSegue(withIdentifier: "ToUser", sender: self)
+                                    if let name = json["displayname"] {
+                                        UserData.shared.myUser.name = name as! String
+                                    }
+                                    let cursus = json["cursus_users"] as! [[String: Any]]
+                                    print("cursus = ", cursus)
                                     
+                                    if let level = cursus.first!["level"] {
+                                        UserData.shared.myUser.level = String(format: "%@", level as! CVarArg)
+                                        print("level = ", level)
+                                    }
+                                    //print(level)
+//                                        if let level = cursus["level"] {
+//                                            UserData.shared.myUser.name = level as! String
+//                                        }
+                                    
+
+                                    print(UserData.shared.myUser)
+                                    DispatchQueue.main.async {
+                                        self.performSegue(withIdentifier: "ToUser", sender: self)
+                                    }
                                 }
                                 catch (let err) {
                                     print("Err: ", err)
@@ -101,6 +102,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        activity.isHidden = true
 
     }
 }
